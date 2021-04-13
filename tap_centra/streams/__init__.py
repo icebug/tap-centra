3ea85554b814ce9caba9c14a5b1241a4e8118efb
Fredrik Ring <fredrikring@MacBook-Pro-som-tillhor-Fredrik.local>
2021-04-13 09:30:14 +0200

    Delete customer schema
    Data warehouse should not save unnecessary personal information.

--- tap_centra/streams/__init__.py
+++ tap_centra/streams/__init__.py
@@ -1,22 +1,18 @@
-
 from tap_centra.streams.orders import OrdersStream
-from tap_centra.streams.customers import CustomersStream
 from tap_centra.streams.products import ProductsStream
 from tap_centra.streams.returns import ReturnsStream
 from tap_centra.streams.stock import StockStream
 
 AVAILABLE_STREAMS = [
     OrdersStream,
-    CustomersStream,
     ProductsStream,
     ReturnsStream,
-    StockStream
+    StockStream,
 ]
 
 __all__ = [
     "OrdersStream",
-    "CustomersStream",
     "ProductsStream",
     "ReturnsStream",
-    "StockStream"
+    "StockStream",
 ]
